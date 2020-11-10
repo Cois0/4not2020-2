cf833f139650e0239fb5ece23ceb94d775ce7786
Cois0 <viniciofs@hotmail.com>
2020-11-10 03:07:50 +0000

    (09/11) botão de excluir funcionando TNC

--- front-end/src/app/pedido/pedido-list/pedido-list.component.ts
+++ front-end/src/app/pedido/pedido-list/pedido-list.component.ts
@@ -1,6 +1,6 @@
-import { MatSnackBar } from '@angular/material/snack-bar';
 import { Component, OnInit } from '@angular/core';
 import { PedidoService } from '../pedido.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-pedido-list',
@@ -8,39 +8,43 @@ import { PedidoService } from '../pedido.service';
   styleUrls: ['./pedido-list.component.scss']
 })
 export class PedidoListComponent implements OnInit {
-  
-  //nome da entidade no plural  
-  pedidos : any = []  
 
-  displayedColumns : String [] = ['num_pedido', 'qtd_pecas', 'data_coleta', 'data_entrega', 'editar', 'excluir']
-  //Injeção de dependência ou inversão de controle
+  // Nome da entidade no plural
+  pedidos : any = []
+
+  // Quais colunas serão exibidas na tabela, e em qual ordem
+  displayedColumns: string[] = ['num_pedido', 'qtd_pecas', 'data_coleta', 'data_entrega', 'editar', 'excluir']
+
+  // Injeção de dependência ou inversão de controle
   constructor(
       private pedidoSrv : PedidoService,
-      private snackBar: MatSnackBar
-      ) { }
+      private snackBar : MatSnackBar
+  ) { }
 
   async ngOnInit() {
-      this.pedidos = await this.pedidoSrv.listar()
-      console.log(this.pedidos)
+    this.pedidos = await this.pedidoSrv.listar()
+    console.log(this.pedidos)
   }
 
   async excluir(id: string) {
-      if(confirm('Deseja realmente excluir?')) {
-        try{
-            await this.pedidoSrv.excluir(id)
-            //1)recarregar os dados da tabela
-            this.ngOnInit()
-            //2) dar feedback para o usuario com mensagem
-            this.snackBar.open('Item excluído com sucesso.', 'Entendi', {​​​​
-            duration: 5000 // 3 segundos
-        }​​​​)
-        }
-        catch(erro) {
-            //3) Dar feedback de erro para o usuario
-            this.snackBar.open('ERRO: não foi possível excluir este item', 'Que pena =/')
-            duration: 5000 // 3 segundos
-            }
-
-        }
+    if(confirm('Deseja realmente excluir?')) {
+      try {
+        await this.pedidoSrv.excluir(id)
+        // 1) Recarregar os dados da tabela
+        this.ngOnInit()
+        // 2) Dar feedback para o usuário com mensagem
+        this.snackBar.open('Item excluído com sucesso.', 'Entendi', {
+          duration: 5000 // 5 segundos
+        })
+      }
+      catch(erro) {
+        // 3) Dar feedback de erro para o usuário
+        this.snackBar.open('ERRO: não foi possível excluir este item.', 'Que pena!', {
+          duration: 5000 // 5 segundos
+        })
+        console.log(erro)
+      }
     }
-  }  
+  }
+
+}
--- front-end/src/app/pedido/pedido.service.ts
+++ front-end/src/app/pedido/pedido.service.ts
@@ -6,22 +6,22 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class PedidoService {
+
   private apiServer : string = environment.apiServer
   private apiUri : string = this.apiServer + 'pedido'
   
   constructor(private http: HttpClient) { }
 
   listar() {
-      return this.http.get(this.apiServer + 'pedido').toPromise()
+      return this.http.get(this.apiUri).toPromise()
   }
 
-  excluir(id:string) {
-      // o metodo nativo do HTTPClient não suporta a passagem de um body para o back-end
+  excluir(id: string) {
+      // o metodo delete() nativo do HTTPClient não suporta a passagem de um body para o back-end
       //return this.http.delete(this.apiServer + 'pedido/' + id)toString.Pomise()
       
       //O método request pode ser usado com qualquer verbo e aceita a passagem de body
-      return this.http.request('DELETE', this.apiServer + 'pedido', {​​body: {​​_id: id}​​}​​).toPromise()
-
-
+      return this.http.request('DELETE', this.apiUri, {​​body: {​​_id: id}​​}​​).toPromise()
   }
+
 }
